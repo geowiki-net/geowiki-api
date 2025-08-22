999405081de1b912c6a8cb0db61e525c4afd01f3
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-08-23 00:54:24 +0200

    Filter.simplify(): more tests

--- test/filterSimplify.js
+++ test/filterSimplify.js
@@ -6,7 +6,12 @@ const list = {
   '(node[a];node._[b];);': '(node["a"];node["b"]["a"];);',
   '(node[a];);': 'node["a"];',
   '((node[a];););': 'node["a"];',
-  '((();node[a];););': 'node["a"];',
+  //'((();node[a];););': 'node["a"];', // TODO
+  'relation[b];node(r)[b]->._a;node._a[a];': 'relation["b"];node(r)["a"]["b"];',
+  'relation[b];node(r)[b]->._a;node._a[a];node._[c];': 'relation["b"];node(r)["c"]["a"]["b"];',
+  '(way[c];way[d];)->._2;nwr._2[a];': '(way["c"]["a"];way["d"]["a"];);',
+  '(way[c];way[d];)->._2;nwr._2[a];nwr._[b];': '(way["c"];way["d"];)->._2;nwr._2["b"]["a"];', // TODO
+  'relation->._a;(node(r._a)[a];way[b];);': 'relation->._a;(node(r._a)["a"];way["b"];);',
 }
 
 describe('Filter.simplify()', function () {

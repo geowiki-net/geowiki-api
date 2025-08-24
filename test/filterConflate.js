487041930e1ae02e6f40401d32ed5b04b8d6f3cc
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-08-24 08:25:24 +0200

    FilterOr: conflate 'or' statements

--- test/filterConflate.js
+++ test/filterConflate.js
@@ -6,6 +6,30 @@ const list = {
     str: 'node["b"]["a"];',
     loki: {"type":{"$eq":"node"},"tags.b":{"$exists":true},"tags.a":{"$exists":true}},
   },
+  'way;node(w);': {
+    str: 'way;node(w);',
+    loki: {"type":{"$eq":"node"}},
+  },
+  'way[a];node(w)->.a;way[a];node.a(w);': {
+    str: 'way["a"]->._1;way["a"];node(w)(w._1);',
+    loki: {"type":{"$eq":"node"}},
+  },
+  '(node[a];node._[b];);': {
+    str: '(node["a"];node["b"]["a"];);',
+    loki: {"$or":[{"type":{"$eq":"node"},"tags.a":{"$exists":true}},{"type":{"$eq":"node"},"tags.b":{"$exists":true},"tags.a":{"$exists":true}}]},
+  },
+  '(node[a];);': {
+    str: 'node["a"];',
+    loki: {"type":{"$eq":"node"},"tags.a":{"$exists":true}},
+  },
+  '((node[a];););': {
+    str: 'node["a"];',
+    loki: {"type":{"$eq":"node"},"tags.a":{"$exists":true}},
+  },
+  //'((();node[a];););': {
+  //  str: 'node["a"];', // TODO
+  //  loki: {"type":{"$eq":"node"},"tags.a":{"$exists":true}},
+  //},
   'relation[b];node(r)[b]->._a;node._a[a];': {
     str: 'relation["b"];node(r)["a"]["b"];',
     loki: {"type":{"$eq":"node"},"tags.a":{"$exists":true},"tags.b":{"$exists":true}},
@@ -14,13 +38,21 @@ const list = {
     str: 'relation["b"];node(r)["c"]["a"]["b"];',
     loki: {"type":{"$eq":"node"},"tags.c":{"$exists":true},"tags.a":{"$exists":true},"tags.b":{"$exists":true}},
   },
-  'way;node(w);': {
-    str: 'way;node(w);',
-    loki: {"type":{"$eq":"node"}},
+  '(way[c];way[d];)->._2;nwr._2[a];': {
+    str: '(way["c"]["a"];way["d"]["a"];);',
+    loki: {"$or":[{"type":{"$eq":"way"},"tags.c":{"$exists":true},"tags.a":{"$exists":true}},{"type":{"$eq":"way"},"tags.d":{"$exists":true},"tags.a":{"$exists":true}}]},
   },
-  'way[a];node(w)->.a;way[a];node.a(w);': {
-    str: 'way["a"]->._1;way["a"];node(w)(w._1);',
-    loki: {"type":{"$eq":"node"}},
+  '(way[c];way[d];)->._2;nwr._2[a];nwr._[b];': {
+    str: '(way["c"]["b"]["a"];way["d"]["b"]["a"];);',
+    loki: {"$or":[{"type":{"$eq":"way"},"tags.c":{"$exists":true},"tags.b":{"$exists":true},"tags.a":{"$exists":true}},{"type":{"$eq":"way"},"tags.d":{"$exists":true},"tags.b":{"$exists":true},"tags.a":{"$exists":true}}]},
+  },
+  'relation->._a;(node(r._a)[a];way[b];);': {
+    str: 'relation->._a;(node(r._a)["a"];way["b"];);',
+    loki: {},
+  },
+  'nwr(48.195,16.335,48.2,16.345)->._base;nwr._base["cuisine"]->._base;(node._base["amenity"="restaurant"];way._base["amenity"="restaurant"];relation._base["amenity"="restaurant"];);nwr._(properties:13);': {
+    str: '(node["amenity"="restaurant"]["cuisine"](48.195,16.335,48.2,16.345)(properties:13);way["amenity"="restaurant"]["cuisine"](48.195,16.335,48.2,16.345)(properties:13);relation["amenity"="restaurant"]["cuisine"](48.195,16.335,48.2,16.345)(properties:13););',
+    loki: {"$or":[{"type":{"$eq":"node"},"tags.amenity":{"$eq":"restaurant"},"tags.cuisine":{"$exists":true},"$and":[{"minlat":{"$lte":48.2},"minlon":{"$lte":16.345},"maxlat":{"$gte":48.195},"maxlon":{"$gte":16.335}}]},{"type":{"$eq":"way"},"tags.amenity":{"$eq":"restaurant"},"tags.cuisine":{"$exists":true},"$and":[{"minlat":{"$lte":48.2},"minlon":{"$lte":16.345},"maxlat":{"$gte":48.195},"maxlon":{"$gte":16.335}}]},{"type":{"$eq":"relation"},"tags.amenity":{"$eq":"restaurant"},"tags.cuisine":{"$exists":true},"$and":[{"minlat":{"$lte":48.2},"minlon":{"$lte":16.345},"maxlat":{"$gte":48.195},"maxlon":{"$gte":16.335}}]}],"needMatch":true},
   },
 }
 

35934dc567970147fe2e77432c182ecba62aaf2e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-11-18 19:45:36 +0100

    Test: run queries against Overpass API, via-server, via-file

---
+++ test/query.js
@@ -0,0 +1,142 @@
+const fs = require('fs')
+const assert = require('assert')
+const async = require('async')
+
+const conf = JSON.parse(fs.readFileSync('test/conf.json', 'utf8'));
+
+const httpLoad = require('../src/httpLoad')
+const OverpassFrontend = require('../src/OverpassFrontend')
+const overpassFrontend = new OverpassFrontend(conf.url)
+
+const queries = [
+  {
+    query: '[out:json];node[place];out;',
+    clearCache: true
+  },
+  {
+    query: '[out:json];node[place];out;',
+  },
+  {
+    query: '[out:json];node[amenity=restaurant];out;'
+  },
+  {
+    query: '[out:json];node[amenity=restaurant];out;out count;way[highway=primary];out body;',
+  },
+  {
+    query: '[out:json];way[building];out geom;'
+  },
+]
+
+describe('Overpass QL Queries - load expected data from Overpass API', function () {
+  async.each(queries, (def, callback) => {
+    it(def.query, function (done) {
+      httpLoad(
+        conf.url,
+        null,
+        def.query,
+        (err, result) => {
+          if (err) {
+            def.expectError = true
+            return done()
+          }
+
+          def.expected = result
+
+          def.expectedElements = [{}]
+          let block = 0
+          def.expected.elements.forEach(el => {
+            const id = el.type + '/' + el.id
+
+            if (el.type === 'count') {
+              //def.expectedElements[block].count = el
+              block++
+              def.expectedElements[block] = {}
+            } else {
+              def.expectedElements[block][id] = el
+            }
+          })
+
+          def.expectedBlocks = block
+          done()
+        }
+      )
+    })
+  })
+})
+
+Object.entries({
+  'via-server': 'from server',
+  'via-file': 'loaded from .osm file'
+}).forEach(([mode, modeText]) => {
+  describe('Overpass QL Queries - ' + modeText, function () {
+    let overpassFrontend
+
+    if (mode === 'via-server') {
+      it('connect to server', function (done) {
+        overpassFrontend = new OverpassFrontend(conf.url)
+        done()
+      })
+    } else {
+      it('wait until file is loaded', function (done) {
+        this.timeout(20000)
+        overpassFrontend = new OverpassFrontend('test/data.osm.bz2')
+        overpassFrontend.once('load', () => done())
+      })
+    }
+
+    async.each(queries, (def, callback) => {
+      it(def.query, function (done) {
+        if (def.clearCache) {
+          overpassFrontend.clearCache()
+        }
+
+        overpassFrontend.query(def.query, {}, (err, result) => {
+          const errors = compareResults(def, result)
+
+          if (errors) {
+            assert.fail(errors.join('\n'))
+          }
+          //assert.deepEqual(def.expected, result)
+
+          done()
+        })
+      })
+    })
+  })
+})
+
+function compareResults (def, actual) {
+  const errors = []
+
+  let block = 0
+  let done = {}
+  actual.elements.forEach(el => {
+    const id = el.type + '/' + el.id
+    done[id] = true
+
+    if (el.type === 'count') {
+      Object.keys(def.expectedElements[block]).forEach(expId => {
+        if (!(expId in done)) {
+          errors.push('Expected element ' + expId + ' missing')
+        }
+      })
+
+      block++
+      done = {}
+    } else if (!(id in def.expectedElements[block])) {
+      errors.push('Unexpected element ' + id)
+    }
+  })
+
+  Object.keys(def.expectedElements[block]).forEach(expId => {
+    if (!(expId in done)) {
+      errors.push('Expected element ' + expId + ' missing')
+    }
+  })
+
+  if (block !== def.expectedBlocks) {
+    errors.push('Wrong count of blocks: ' + block + ' ' + def.expectedBlocks)
+  }
+
+  return errors.length ? errors : null
+}

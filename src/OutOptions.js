6de4dac4be1619f78254974055b591c51710886d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-01-11 07:08:25 +0100

    OutOptions: move calculation of out options from OutStatement

---
+++ src/OutOptions.js
@@ -0,0 +1,83 @@
+const OverpassFrontend = require('./defines')
+
+const outParams = {
+  ids: OverpassFrontend.ID_ONLY,
+  skel: OverpassFrontend.MEMBERS,
+  body: OverpassFrontend.MEMBERS | OverpassFrontend.TAGS,
+  tags: OverpassFrontend.TAGS,
+  meta: OverpassFrontend.MEMBERS | OverpassFrontend.TAGS | OverpassFrontend.META,
+  geom: OverpassFrontend.GEOM | OverpassFrontend.MEMBERS | OverpassFrontend.TAGS,
+  bb: OverpassFrontend.BBOX | OverpassFrontend.TAGS | OverpassFrontend.MEMBERS,
+  center: OverpassFrontend.CENTER | OverpassFrontend.TAGS | OverpassFrontend.MEMBERS
+}
+const outOtherParams = {
+  asc: 0, // TODO
+  qt: 0, // TODO
+  count: 0,
+  noids: 0
+}
+
+module.exports = class OutOptions {
+  constructor (value) {
+    this.def = { out: value }
+  }
+
+  outOptions () {
+    const result = {}
+    let hasParams = false
+
+    this.def.out.forEach(outParam => {
+      if (outParam in outParams) {
+        result[outParam] = true
+        hasParams = true
+      } else if (outParam.match(/^[0-9]+$/)) {
+        ; // ignore
+      } else if (outParam in outOtherParams) {
+        result[outParam] = true
+      } else {
+        throw new Error('Invalid parameter for print: "' + outParam + '"')
+      }
+    })
+
+    if (result.count) {
+      result.ids = true
+      hasParams = true
+    }
+
+    if (!hasParams) {
+      result.body = true
+    }
+
+    return result
+  }
+
+  properties () {
+    let result = 0
+    let hasParams = false
+    const otherParams = {}
+
+    this.def.out.forEach(outParam => {
+      if (outParam in outParams) {
+        result |= outParams[outParam]
+        hasParams = true
+      } else if (outParam.match(/^[0-9]+$/)) {
+        ; // ignore
+      } else if (outParam in outOtherParams) {
+        otherParams[outParam] = true
+      } else {
+        throw new Error('Invalid parameter for print: "' + outParam + '"')
+      }
+    })
+
+    if (otherParams.count) {
+      result |= OverpassFrontend.ID_ONLY
+      hasParams = true
+    }
+
+    if (!hasParams) {
+      result = OverpassFrontend.MEMBERS | OverpassFrontend.TAGS
+    }
+
+    return result
+  }
+}

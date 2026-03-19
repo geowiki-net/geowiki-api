87cfe9070f966035cb2f885b353ac29d46e252c4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-03-19 21:27:17 +0100

    geojson2elements: move code from fileFormatGeoJSON()

--- src/fileFormatGeoJSON.js
+++ src/fileFormatGeoJSON.js
@@ -1,8 +1,4 @@
-const elementIds = {
-  node: 0,
-  way: 0,
-  relation: 0
-}
+const geojson2elements = require('./geojson2elements.js')
 
 module.exports = {
   id: 'GeoJSON',
@@ -27,72 +23,4 @@ module.exports = {
 
     callback(null, result)
   }
-}
-
-function geojson2elements (data, elements, options) {
-  if (data.type === 'FeatureCollection') {
-    return data.features.forEach(feature => {
-      geojson2elements(feature, elements, options)
-    })
-  }
-
-  if (data.type !== 'Feature') {
-    throw new Error('Unknown type ' + data.type)
-  }
-
-  let element
-
-  switch (data.geometry.type) {
-    case 'Point':
-      element = {
-        type: 'node',
-        lon: data.geometry.coordinates[0],
-        lat: data.geometry.coordinates[1]
-      }
-      break
-    case 'LineString':
-      element = {
-        type: 'way',
-        geometry: data.geometry.coordinates.map(c => {
-          return { lon: c[0], lat: c[1] }
-        })
-      }
-      break
-    case 'Polygon':
-      if (data.geometry.coordinates.length === 1) {
-        element = {
-          type: 'way',
-          geometry: data.geometry.coordinates[0].map(c => {
-            return { lon: c[0], lat: c[1] }
-          })
-        }
-      } else {
-        element = {
-          type: 'relation',
-          members: data.geometry.coordinates.map((ring, i) => {
-            return {
-              type: 'way',
-              ref: --elementIds.way,
-              role: i ? 'inner' : 'outer',
-              geometry: ring.map(c => {
-                return { lon: c[0], lat: c[1] }
-              })
-            }
-          })
-        }
-        data.properties = { ...data.properties, type: 'multipolygon' }
-      }
-      break
-    default:
-      console.log('Unknown geometry type ' + data.geometry.type)
-      return
-  }
-
-  if (data.properties) {
-    element.tags = data.properties
-  }
-
-  element.id = --elementIds[element.type]
-
-  elements.push(element)
 }
---
+++ src/geojson2elements.js
@@ -0,0 +1,75 @@
+const elementIds = {
+  node: 0,
+  way: 0,
+  relation: 0
+}
+
+function geojson2elements (data, elements, options) {
+  if (data.type === 'FeatureCollection') {
+    return data.features.forEach(feature => {
+      geojson2elements(feature, elements, options)
+    })
+  }
+
+  if (data.type !== 'Feature') {
+    throw new Error('Unknown type ' + data.type)
+  }
+
+  let element
+
+  switch (data.geometry.type) {
+    case 'Point':
+      element = {
+        type: 'node',
+        lon: data.geometry.coordinates[0],
+        lat: data.geometry.coordinates[1]
+      }
+      break
+    case 'LineString':
+      element = {
+        type: 'way',
+        geometry: data.geometry.coordinates.map(c => {
+          return { lon: c[0], lat: c[1] }
+        })
+      }
+      break
+    case 'Polygon':
+      if (data.geometry.coordinates.length === 1) {
+        element = {
+          type: 'way',
+          geometry: data.geometry.coordinates[0].map(c => {
+            return { lon: c[0], lat: c[1] }
+          })
+        }
+      } else {
+        element = {
+          type: 'relation',
+          members: data.geometry.coordinates.map((ring, i) => {
+            return {
+              type: 'way',
+              ref: --elementIds.way,
+              role: i ? 'inner' : 'outer',
+              geometry: ring.map(c => {
+                return { lon: c[0], lat: c[1] }
+              })
+            }
+          })
+        }
+        data.properties = { ...data.properties, type: 'multipolygon' }
+      }
+      break
+    default:
+      console.log('Unknown geometry type ' + data.geometry.type)
+      return
+  }
+
+  if (data.properties) {
+    element.tags = data.properties
+  }
+
+  element.id = --elementIds[element.type]
+
+  elements.push(element)
+}
+
+module.exports = geojson2elements
